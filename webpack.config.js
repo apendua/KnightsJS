451193767a3518e2bc650e10ce3119c3d3597acd
Tomasz Lenarcik <apendua@gmail.com>
2016-06-11 16:29:01 +0200

    Build a static website for demo

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: [ 'babel-polyfill', './demo/main.jsx' ],
   output: {
-    path: './build',
+    path: '.',
     filename: 'bundle.js',
     publicPath: '/build/',
   },
